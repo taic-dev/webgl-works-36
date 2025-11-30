921cebfc2d4f154970d3228f18f5b9dd5d6cd498
ohmoto <c.t.o.taishi.0530@gmail.com>
2025-11-30 12:00:54 +0900

    add: rainbow ring

--- src/assets/shader/mv/fragmentShader.glsl
+++ src/assets/shader/mv/fragmentShader.glsl
@@ -10,20 +10,35 @@ varying vec3 vPosition;
 #pragma glslify: noise2d = require('glsl-noise/simplex/2d');
 #pragma glslify: noise3d = require('glsl-noise/simplex/3d');
 
+vec3 hsv2rgb(vec3 c) {
+  vec4 K = vec4(1.0, 2.0/3.0, 1.0/3.0, 3.0);
+  vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+  return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
 void main() {
   vec2 uv = gl_FragCoord.xy / uResolution.xy;
   uv = uv - 0.5;
   uv.x *= (uResolution.x / uResolution.y);
 
   float d = length(uv);
-  float s = step(0.1, d);
 
-  vec4 red = vec4(1., 0., 0., 1.);
-  vec4 green = vec4(0., 1., 0., 1.);
-  vec4 blue = vec4(0., 0., 1., 1.);
 
-  vec4 mixRedGreen = mix(red, green, 0.5);
-  vec4 color = mix(blue, mixRedGreen, 0.5);
+  float edge1 = smoothstep(0.0, 0.5, d);
+  float edge2 = smoothstep(0.4, 0.5, d);
+  float ring = edge1 - edge2;
 
-  gl_FragColor = vec4(vec3(s), 1.);
+  float n = noise2d(uv);
+  float nEdge1 = smoothstep(0.5, 0.2, n);
+  float nEdge2 = smoothstep(0.2, 0.3, n);
+  float nRing = nEdge1 - nEdge2;
+
+  float h = (edge1);
+  float s = (edge1 * 0.2);
+  float l = 1.;
+  vec3 rainbow = hsv2rgb(vec3(h, s, l));
+  float edge3 = smoothstep(0.1, 0.2, n);
+
+
+  gl_FragColor = vec4(vec3(rainbow), 1.);
 }
